a3767da49b9256b755bca08e58ee1f91a75f312b
Kurobane Yan <yan.kurobane.@gmail.com>
2017-05-17 08:34:15 +0300

    remove redundant index analogs

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,6 @@ let url = 'http://localhost:1337/api/cars/mostPopular/25'
 
 let viewModel = kendo.observable({
   cars: new kendo.data.DataSource({
-    batch: true,
     transport: {
       read: {
         url: url,
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const config = {
   context: path.resolve(__dirname, 'public/'),
   entry: {
     index: './js/index',
-    index2: './js/index2'
   },
   output: {
     path: path.resolve(__dirname, 'public/dist'),
