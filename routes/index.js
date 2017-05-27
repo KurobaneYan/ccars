ec23bb7bc5e9910390f331605e84e113bac154bb
Kurobane Yan <yan.kurobane.@gmail.com>
2017-05-27 14:30:58 +0300

    temp commit(backup perpose)

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 
 let router = express.Router()
 
+/*
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'CCars' })
 })
+*/
 
 router.get('/filter', (req, res, next) => {
   res.render('filter', { title: 'CCars Filter' })
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,47 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+  source: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'public')
+}
 
 const config = {
-  context: path.resolve(__dirname, 'public/'),
+  context: PATHS.source,
   entry: {
-    index: './js/index'
+    index: './js/index.js'
   },
   output: {
-    path: path.resolve(__dirname, 'public/dist'),
+    path: PATHS.build,
     filename: '[name].js'
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['babili']
+        }
+      }
+    ],
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: PATHS.source + '/index.pug'
+    })
+  ],
+  devtool: 'source-map'
 }
 
 module.exports = config
