2e72c81e9639cefd6682c627a10b01d098e6f2be
Kurobane Yan <yan.kurobane@gmail.com>
2017-05-27 23:00:41 +0300

    fix bug in filter, change layout

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,79 +1,48 @@
-$(function () {
-  const manufacturersUrl = 'http://localhost:3000/api/manufacturers/'
-  const baseModelsUrl = 'http://localhost:3000/api/models/'
+const manufacturersUrl = 'http://localhost:3000/api/manufacturers/'
+const baseModelsUrl = 'http://localhost:3000/api/models/'
 
-  const manufacturersData = new kendo.data.DataSource({
-    transport: {
-      read: {
-        url: manufacturersUrl,
-        dataType: 'json'
-      }
+const manufacturersData = new kendo.data.DataSource({
+  transport: {
+    read: {
+      url: manufacturersUrl,
+      dataType: 'json'
     }
-  })
+  }
+})
 
-  let modelsData = new kendo.data.DataSource({
-    transport: {
-      read: {
-        url: function () {
-          return baseModelsUrl + filter.get('selectedManufacturer')
-        },
-        dataType: 'json'
-      }
+const modelsData = new kendo.data.DataSource({
+  transport: {
+    read: {
+      url: function () {
+        return baseModelsUrl + filterViewModel.get('selectedManufacturer')
+      },
+      dataType: 'json'
     }
-  })
+  }
+})
 
-  let filter = kendo.observable({
-    selectedManufacturer: null,
-    years: [1996, 2017],
-    price: [0, 1000000],
-    transmission: 'Manual',
-    fuelType: 'Gasoline',
-    kilometrage: null,
-    engineDisplacement: null,
-    manufacturers: manufacturersData,
-    manufacturerOnChange: function () {
-      const manufacturer = this.get('selectedManufacturer')
-      if ((manufacturer !== null) && (manufacturer !== '')) {
-        this.set('isModelEnabled', true)
-        this.models.read()
-      } else {
-        this.set('isModelEnabled', false)
-      }
-    },
-    isModelEnabled: false,
-    selectedModel: null,
-    models: modelsData,
-    yearSliderOnSlide: function (e) {
-      console.log('on slide ', e.value.toString())
+export const filterViewModel = kendo.observable({
+  selectedManufacturer: null,
+  years: [1996, 2017],
+  price: [0, 1000000],
+  transmission: 'Manual',
+  fuelType: 'Gasoline',
+  kilometrage: null,
+  engineDisplacement: null,
+  manufacturers: manufacturersData,
+  manufacturerOnChange: function () {
+    const manufacturer = this.get('selectedManufacturer')
+    if ((manufacturer !== null) && (manufacturer !== '')) {
+      this.set('isModelEnabled', true)
+      this.models.read()
+    } else {
+      this.set('isModelEnabled', false)
     }
-  })
-
-  kendo.bind($('#filter'), filter)
-
-  $('#showAlert').click(function () {
-    const manufacturer = filter.get('selectedManufacturer')
-    const model = filter.get('selectedModel')
-    const years = filter.get('years')
-    const price = filter.get('price')
-    const kilometrage = filter.get('kilometrage')
-    const displacement = filter.get('engineDisplacement')
-    let msg = ''
-    if (manufacturer !== null) {
-      msg += 'manufacturer ' + manufacturer + '\n'
-    }
-    if (model !== null) {
-      msg += 'model ' + model + '\n'
-    }
-    msg += 'year from ' + years[0] + ' to ' + years[1] + '\n'
-    msg += 'price from ' + price[0] + ' to ' + price[1] + '\n'
-    msg += 'transmission ' + filter.get('transmission') + '\n'
-    msg += 'fuel type ' + filter.get('fuelType') + '\n'
-    if (kilometrage !== null) {
-      msg += 'kilometrage ' + kilometrage + '\n'
-    }
-    if (displacement !== null) {
-      msg += 'engine displacenemt ' + displacement + '\n'
-    }
-    alert(msg)
-  })
+  },
+  isModelEnabled: false,
+  selectedModel: null,
+  models: modelsData,
+  yearSliderOnSlide: function (e) {
+    console.log('on slide ', e.value.toString())
+  }
 })
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import { filterViewModel } from './filter.js'
+
 const url = 'http://localhost:3000/api/cars/most-popular/25'
 
 const viewModel = kendo.observable({
@@ -12,11 +14,15 @@ const viewModel = kendo.observable({
 })
 
 const layout = new kendo.Layout('layout')
-const index = new kendo.View('index', {
-  model: viewModel
-})
+const index = new kendo.View('index', { model: viewModel })
+const filter = new kendo.View('filter', { model: filterViewModel })
+const routeMissing = new kendo.View('routeMissing')
 
-const router = new kendo.Router()
+const router = new kendo.Router({
+  routeMissing: (e) => {
+    layout.showIn('#content', routeMissing)
+  }
+})
 
 router.bind('init', () => {
   layout.render($('#app'))
@@ -24,6 +30,10 @@ router.bind('init', () => {
 
 router.route('/', () => {
   layout.showIn('#content', index)
+})
+
+router.route('/filter', () => {
+  layout.showIn('#content', filter)
 })
 
 $(() => {
