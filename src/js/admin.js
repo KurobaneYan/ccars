969763a63db3f1f21ce9d78bdf04ad679f78d8f8
Kurobane Yan <yan.kurobane@gmail.com>
2017-05-30 22:44:49 +0300

    add validation for admin popup

--- src/js/admin.js
+++ src/js/admin.js
@@ -26,15 +26,82 @@ const carData = new kendo.data.DataSource({
     model: {
       id: '_id',
       fields: {
-        manufacturer: { type: 'string' },
-        model: { type: 'string' },
-        year: { type: 'number' },
-        kilometrage: { type: 'number' },
-        price: { type: 'number' },
-        transmissionType: { type: 'string' },
-        fuelType: { type: 'string' },
-        engineDisplacement: { type: 'number' },
-        views: { type: 'number' },
+        manufacturer: {
+          type: 'string',
+          validation: {
+            required: true,
+            manufacturervalidation: (input) => {
+              if (input.is('[name="manufacturer"]') && input.val() !== '') {
+                input.attr('data-manufacturervalidation-msg', 'Manufacturer name should start with capital letter')
+                return /^[A-Z]/.test(input.val())
+              }
+              return true
+            }
+          },
+          defaultValue: 'Mitsubishi'
+        },
+        model: {
+          type: 'string',
+          modelvalidation: {
+            required: true,
+            modelvalidation: (input) => {
+              if (input.is('[name="model"]') && input.val() !== '') {
+                input.attr('data-modelvalidation-msg', 'Model name should start with capital letter')
+                return /^[A-Z]/.test(input.val())
+              }
+              return true
+            }
+          },
+          defaultValue: 'Lancer'
+        },
+        year: {
+          type: 'number',
+          validation: { required: true, min: 1996, max: 2017 }
+        },
+        kilometrage: {
+          type: 'number',
+          validation: { required: true, min: 1, max: 1000 }
+        },
+        price: {
+          type: 'number',
+          validation: { required: true, min: 1, max: 1000000 }
+        },
+        transmissionType: {
+          type: 'string',
+          validation: {
+            required: true,
+            transmissionvalidation: (input) => {
+              if (input.is('[name="transmissionType"]') && input.val() !== '') {
+                input.attr('data-transmissionvalidation-msg', 'Transmission type should start with capital letter')
+                return /^[A-Z]/.test(input.val())
+              }
+              return true
+            }
+          },
+          defaultValue: 'Manual'
+        },
+        fuelType: {
+          type: 'string',
+          validation: {
+            required: true,
+            fuelvalidation: (input) => {
+              if (input.is('[name="fuelType"]') && input.val() !== '') {
+                input.attr('data-fuelvalidation-msg', 'Fuel type should start with capital letter')
+                return /^[A-Z]/.test(input.val())
+              }
+              return true
+            }
+          },
+          defaultValue: 'Gasoline'
+        },
+        engineDisplacement: {
+          type: 'number',
+          validation: { required: true, min: 1, max: 10000 }
+        },
+        views: {
+          type: 'number',
+          validation: { required: true, min: 0 }
+        },
         photos: {
           defaultValue: ['https://img.memecdn.com/404-car-not-found_c_6767609.jpg']
         }
